7944c5c4e01444d837a9367f660c8476c7b7eb6f
cinarece <ece.cinar@ace.games>
2024-07-17 12:03:26 +0300

    edit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,55 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-   [SerializeField] float runSpeed = 10f;
-    Vector2 moveInput;
-    Rigidbody2D myRigidbody;
+    [SerializeField] private float runSpeed = 10f;
+    private Vector2 moveInput;
+    private Rigidbody2D myRigidbody;
 
     void Start()
     {
-        myRigidbody= GetComponent<Rigidbody2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        ProcessInputs();
         Run();
         FlipSprite();
-
     }
 
-     void OnMove(InputValue value)
+    private void ProcessInputs()
     {
-      moveInput=value.Get<Vector2>();
-      Debug.Log(moveInput);
-    }
-     
-     void Run()
-     {
-       Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y );
-       myRigidbody.velocity = playerVelocity;
+        moveInput = Vector2.zero;
 
-     }
-
-     void FlipSprite()
-    {
-        bool playerHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x)> Mathf.Epsilon;
-        
-        if (playerHorizontalSpeed)
-
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-        
-        transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x),1f);
+            moveInput.y = 1;
         }
-        
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            moveInput.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveInput.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            moveInput.x = 1;
+        }
+
+        moveInput = moveInput.normalized; // Hareketi normalize et
+        Debug.Log(moveInput);
     }
 
+    private void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
 
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
 
-
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
 }
